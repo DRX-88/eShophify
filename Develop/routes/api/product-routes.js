b4de1f4b5ff85617f5a8ca8d3c4f3fe69af86f32
DRX-88 <nrmj02@gmail.com>
2024-05-23 22:29:36 -0400

    checked that everything is working

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -4,22 +4,24 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', async (req, res) => {
-  try {
-    const productData = await Product.findAll({
-      include: [{ model: Category }, { model: Tag, through: ProductTag, as: 'product_tags' }],
-    });
-    res.status(200).json(productData);
-  } catch (err) {
-    res.status(500).json(err);
+  router.get('/', async (req, res) => {
+    try {
+      const productData = await Product.findAll({
+        include: [{ model: Category }, { model: Tag, through: ProductTag, as: 'tags' }],
+      });
+      res.status(200).json(productData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
   }
-});
+  );
 
 // get one product
 router.get('/:id', async (req, res) => {
    try {
     const productData = await Product.findByPk(req.params.id, {
-      include: [{ model: Category }, { model: Tag, through: ProductTag, as: 'product_tags' }],
+      include: [{ model: Category, attributes: ['id', 'category_name'] }, 
+      { model: Tag, attributes: ['id', 'tag_name'] }],
     });
 
     if (!productData) {
@@ -35,25 +37,16 @@ router.get('/:id', async (req, res) => {
 
 // create new product
 router.post('/', async (req, res) => {
-  try {
+  try { 
     const productData = await Product.create(req.body);
-    if (req.body.tagIds.length) {
-      const productTagIdArr = req.body.tagIds.map((tag_id) => {
-        return {
-          product_id: productData.id,
-          tag_id,
-        };
-      });
-      const productTagIds = await ProductTag.bulkCreate(productTagIdArr);
-      res.status(200).json(productTagIds);
-    } else {
-      res.status(200).json(productData);
-    }
+    res.status(200).json(productData);
   } catch (err) {
     res.status(400).json(err);
-  }
+  } 
 }
 );
+
+  
 
 
 
